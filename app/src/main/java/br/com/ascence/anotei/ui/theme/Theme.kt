ea76860567ab66f2ae44ad9790acd4734a915e50
Jessica Silva <jessica.silva.appdev@gmail.com>
2023-09-19 20:08:59 -0300

    chore: setting app theme colors (#2)
    * chore: gradle xml
    
    * chore: setting up app theme colors
    
    ---------
    
    Co-authored-by: Jessica <jessica.silva.appdev@gamil.com>

--- app/src/main/java/br/com/ascence/anotei/ui/theme/Theme.kt
+++ app/src/main/java/br/com/ascence/anotei/ui/theme/Theme.kt
@@ -13,28 +13,21 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = darkPrimary,
+    secondary = darkSecondary,
+    tertiary = darkTerteary,
+    error = darkErrorColor,
+    background = darkPrimaryBackgroundColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = lightPrimary,
+    secondary = lightSecondary,
+    tertiary = lightTertiary,
+    error = lightErrorColor,
+    background = lightPrimaryBackgroundColor
 )
 
 @Composable
@@ -43,21 +36,22 @@ fun AnoteiTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    val (colorScheme, statusBarColor) = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) Pair(dynamicDarkColorScheme(context), darkStatusBarColor)
+            else Pair(dynamicLightColorScheme(context), lightStatusBarColor)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> Pair(DarkColorScheme, darkStatusBarColor)
+        else -> Pair(LightColorScheme, lightStatusBarColor)
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = statusBarColor.toArgb()
         }
     }
 
