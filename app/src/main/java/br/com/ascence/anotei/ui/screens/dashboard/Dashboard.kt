760ae0ead0b86b13f575c844e35166632a215ac6
Jessica Silva <jessica.silva.appdev@gmail.com>
2023-09-21 23:30:16 -0300

    chore: updating theme system (#5)
    * chore: gradle xml
    
    * updating app theme system
    
    ---------
    
    Co-authored-by: Jessica <jessica.silva.appdev@gamil.com>

--- app/src/main/java/br/com/ascence/anotei/ui/screens/dashboard/Dashboard.kt
+++ app/src/main/java/br/com/ascence/anotei/ui/screens/dashboard/Dashboard.kt
@@ -1,14 +1,40 @@
 package br.com.ascence.anotei.ui.screens.dashboard
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.ascence.anotei.ui.theme.AnoteiAppTheme
 import br.com.ascence.anotei.ui.theme.AnoteiTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Dashboard(modifier: Modifier = Modifier) {
-    Text("Dashboard")
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { _ ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(AnoteiAppTheme.colors.colorScheme.background)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = "Dashboard",
+                fontSize = AnoteiAppTheme.fontSizes.medium,
+                color = AnoteiAppTheme.colors.primaryTextColor
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
