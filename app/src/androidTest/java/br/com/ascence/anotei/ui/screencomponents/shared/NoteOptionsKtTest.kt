6c44490bd3fd1d922ee5c99a4e1fe176983c4fc3
Jessica Silva <jessica.silva.appdev@gmail.com>
2023-10-03 20:52:42 -0300

    chore: bottom app bar UI test (#14)
    * chore: gradle xml
    
    * chore: extracting bottom app bar to a separated component to be used in other screens
    
    * chore: creating NoteOptions ui test class
    
    ---------
    
    Co-authored-by: Jessica <jessica.silva.appdev@gamil.com>

---
+++ app/src/androidTest/java/br/com/ascence/anotei/ui/screencomponents/shared/NoteOptionsKtTest.kt
@@ -0,0 +1,45 @@
+package br.com.ascence.anotei.ui.screencomponents.shared
+
+import androidx.compose.ui.test.assertContentDescriptionContains
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import br.com.ascence.anotei.model.NoteOptionsPresentationType
+import br.com.ascence.anotei.ui.theme.AnoteiTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NoteOptionsKtTest {
+
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    private fun setup(
+        optionType: NoteOptionsPresentationType,
+    ) {
+        composeTestRule.setContent {
+            AnoteiTheme {
+                OptionMainAction(
+                    optionType = optionType,
+                    onFABClick = {}
+                )
+            }
+        }
+    }
+
+    @Test
+    fun verifyConfirmationIconAccordingToPresentationType() {
+        setup(NoteOptionsPresentationType.EDIT_MODE)
+        composeTestRule.onNodeWithTag(FAB_ICON_TEST_TAG, useUnmergedTree = true)
+            .assertContentDescriptionContains("Confirmar alterações")
+    }
+
+    @Test
+    fun verifyCEditIconAccordingToPresentationType() {
+        setup(NoteOptionsPresentationType.PREVIEW_MODE)
+        composeTestRule.onNodeWithTag(FAB_ICON_TEST_TAG, useUnmergedTree = true)
+            .assertContentDescriptionContains("Alterar nota")
+    }
+}
