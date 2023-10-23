7f4bf6529aa76c8aeef18846f07b9b56771d2a0b
Jessica Silva <jessica.silva.appdev@gmail.com>
2023-10-23 18:32:25 -0300

    chore: setting up navigation structure (#23)
    * chore: gradle xml
    
    * chore: setting up navigation initial structure, updating dashboard structure
    
    ---------
    
    Co-authored-by: Jessica <jessica.silva.appdev@gamil.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/br/com/ascence/anotei/MainActivity.kt
+++ app/src/main/java/br/com/ascence/anotei/MainActivity.kt
@@ -3,7 +3,13 @@ package br.com.ascence.anotei
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.ascence.anotei.navigation.DASHBOARD_PATH
+import br.com.ascence.anotei.navigation.NOTE_PATH
 import br.com.ascence.anotei.ui.screens.dashboard.Dashboard
+import br.com.ascence.anotei.ui.screens.note.NoteScreen
 import br.com.ascence.anotei.ui.theme.AnoteiTheme
 import br.com.ascence.anotei.ui.theme.darkThemeColors
 
@@ -11,8 +17,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val controller = rememberNavController()
+
             AnoteiTheme(darkColors = darkThemeColors()) {
-                Dashboard()
+                NavHost(navController = controller, startDestination = DASHBOARD_PATH) {
+                    composable(DASHBOARD_PATH) { Dashboard(navController = controller) }
+                    composable(NOTE_PATH) { NoteScreen() }
+                }
             }
         }
     }
