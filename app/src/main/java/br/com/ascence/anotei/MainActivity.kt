d4b6c251cb3b93e3872b30334a80f3755c6061a1
Jessica Silva <jessica.silva.appdev@gmail.com>
2023-10-30 23:03:40 -0300

    chore:note newnote setup (#26)
    * chore: gradle xml
    
    * chore: removing unused methods
    
    * chore: removing unused methods
    
    ---------
    
    Co-authored-by: Jessica <jessica.silva.appdev@gamil.com>

--- app/src/main/java/br/com/ascence/anotei/MainActivity.kt
+++ app/src/main/java/br/com/ascence/anotei/MainActivity.kt
@@ -3,6 +3,11 @@ package br.com.ascence.anotei
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,12 +22,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val slideEffect = spring<IntOffset>(dampingRatio = Spring.DampingRatioMediumBouncy)
+
             val controller = rememberNavController()
 
             AnoteiTheme(darkColors = darkThemeColors()) {
                 NavHost(navController = controller, startDestination = DASHBOARD_PATH) {
                     composable(DASHBOARD_PATH) { Dashboard(navController = controller) }
-                    composable(NOTE_PATH) { NoteScreen() }
+                    composable(
+                        route = NOTE_PATH,
+                        enterTransition = {
+                            slideInHorizontally(
+                                initialOffsetX = { 1000 },
+                                animationSpec = slideEffect
+                            )
+                        },
+                        exitTransition = {
+                            slideOutHorizontally(
+                                targetOffsetX = { -1000 },
+                                animationSpec = slideEffect
+                            )
+                        },
+                        popEnterTransition = {
+                            slideInHorizontally(
+                                initialOffsetX = { -1000 },
+                                animationSpec = slideEffect
+                            )
+                        },
+                        popExitTransition = {
+                            slideOutHorizontally(
+                                targetOffsetX = { 1000 },
+                                animationSpec = slideEffect
+                            )
+                        }
+                    ) { NoteScreen(navController = controller) }
                 }
             }
         }
