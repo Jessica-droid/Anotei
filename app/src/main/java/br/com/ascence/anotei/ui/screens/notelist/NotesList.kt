264af6aa5cf80d2398c899b81b042ebbb6daa8db
Jessica <jessica.silva.appdev@gamil.com>
2023-11-04 20:51:00 -0300

    chore: refactoring note object to provide other note types

--- app/src/main/java/br/com/ascence/anotei/ui/screens/notelist/NotesList.kt
+++ app/src/main/java/br/com/ascence/anotei/ui/screens/notelist/NotesList.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.Modifier
 import br.com.ascence.anotei.data.mock.notesListMock
 import br.com.ascence.anotei.data.preview.ColorSchemePreviews
 import br.com.ascence.anotei.model.Note
-import br.com.ascence.anotei.ui.screens.dashboard.components.notecard.NoteCard
+import br.com.ascence.anotei.ui.screens.notelist.components.notecard.NoteCard
 import br.com.ascence.anotei.ui.theme.AnoteiAppTheme
 import br.com.ascence.anotei.ui.theme.AnoteiTheme
 
